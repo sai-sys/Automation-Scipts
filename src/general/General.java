fd11d92dfd73c4dd497ab9289a4e21ef83a46d08
karthik-tasteguru <karthik.azzarapu@tasteguru.ai>
2019-12-12 12:23:04 -0500

    Webdriver path adjusted to relative path

--- src/general/General.java
+++ src/general/General.java
@@ -9,7 +9,8 @@ import global.Global;
 public class General extends Global{
 		
 		public void startUP() {
-			System.setProperty("webdriver.chrome.driver", "F:\\Programming\\Automation Testing\\Selenium\\chromedriver.exe");
+			//System.setProperty("webdriver.chrome.driver", "F:\\Programming\\Automation Testing\\Selenium\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver","lib/chromedriver.exe");
 			driver = new ChromeDriver();
 		}		
 		
